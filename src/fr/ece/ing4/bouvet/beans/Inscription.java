effd5ac2edccc8b3c25c71591a75dbe64b7b47e1
Bouveti <bouveti94@gmail.com>
2015-11-17 14:51:20 +0100

    Inscription.role

--- src/fr/ece/ing4/bouvet/beans/Inscription.java
+++ src/fr/ece/ing4/bouvet/beans/Inscription.java
@@ -4,12 +4,14 @@ public class Inscription {
 	private int id;
 	private int eleve;
 	private int module;
+	private String role;
 	
-	public Inscription(int id, int eleve, int module) {
+	public Inscription(int id, int eleve, int module, String role) {
 		super();
 		this.id = id;
 		this.eleve = eleve;
 		this.module = module;
+		this.role = role;
 	}
 
 	public int getId() {
@@ -34,5 +36,13 @@ public class Inscription {
 
 	public void setModule(int module) {
 		this.module = module;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
 	}
 }
