a47f45489abe9297c9c6806ca56f2cd7e2ed8ba5
Stoumfou <alexis.pambourg@gmail.com>
2015-11-19 20:15:15 +0100

    Multi Utilisateur OK

--- src/fr/ece/ing4/bouvet/test/Server.java
+++ src/fr/ece/ing4/bouvet/test/Server.java
@@ -9,7 +9,24 @@ public class Server {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 			
-		ServerSocket socketserver  ;
+		ServerSocket s = new ServerSocket(2009);
+		while(true){
+			Socket connex = s.accept();
+			System.out.println("Hello T'es Co GG !");
+		}
+			/*new Thread(new Evaluateur(connex)).start();
+			DataInputStream dis = new DataInputStream(connex.getInputStream());
+			DataOutputStream dos = new DataOutputStream(connex.getOutputStream());
+
+			int a = dis.readInt();
+			char op = dis.readChar();
+			int b = dis.readInt();
+			
+			dis.close();
+			dos.close();*/
+		
+		
+		/*ServerSocket socketserver  ;
 		Socket socketduserveur ;
 		socketserver = new ServerSocket(2009);
 		try {
@@ -21,6 +38,6 @@ public class Server {
 
 		}catch (IOException e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 }
